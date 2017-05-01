0ea9a8ff9346286504efea14147dafff547833a7
Guoxiang <gzhang355@gmail.com>
2017-05-01 15:54:15 -0700

    change to raw depth

--- python/receiver.py
+++ python/receiver.py
@@ -5,8 +5,8 @@ import numpy as np
 HOST = '10.31.48.204'    # Change this to the IP address of your Aero
 
 
-def process_average_depth(average_depth):
-     print "Average depth: ", average_depth 
+def process_average_depth(adepth):
+     print "depth at [240,320]: ", depth[240,320] 
      # Fill in your code here
 
 def get_rgb_depth_time(byte_data):
@@ -15,11 +15,10 @@ def get_rgb_depth_time(byte_data):
     depth_size = unpack('i', byte_data[12:16])[0] 
     rgb = np.fromstring(byte_data[16:16+rgb_size], dtype=np.uint8)
     depth = np.fromstring(byte_data[16+rgb_size:16+rgb_size+depth_size], dtype=np.uint16)
-    average_depth = depth.sum()/(depth != 0).sum()
     #print depth[200*200: 200*203]
     depth = depth.reshape((480, 640))
     rgb = rgb.reshape((480,640,3))
-    return rgb, depth, time_stamp, average_depth
+    return rgb, depth, time_stamp
 
 
 
@@ -37,6 +36,6 @@ while True:
     s.shutdown(1)
     s.close()
     print "Received one frame"
-    rgb, depth, time_stamp, average_depth = get_rgb_depth_time(byte_data)
-    process_average_depth(average_depth)
+    rgb, depth, time_stamp = get_rgb_depth_time(byte_data)
+    process_average_depth(depth)
 
